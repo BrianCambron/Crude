e8492bf2d29c00b62de6c7bc716f7a6dc1fd206a
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-22 19:52:53 -0400

    have my delete method written out but does not work

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const BASE_URL ='http://tiny-lasagna-server.herokuapp.com/collections/cohort-covid';
+const BASE_URL = 'http://tiny-lasagna-server.herokuapp.com/collections/cohort-covid';
 
 const $container = document.querySelector('ul');
 let html = '';
-function buildHTML(data){
-  data.forEach(function(item){
+
+function buildHTML(data) {
+  data.forEach(function(item) {
     console.log('item', item);
-    html += `<li>${item.message}</li>`
+    html += `<li><span>${item.name}</span>-${item.message} <button onclick="deleteBtn(${item._id})">Delete</button></li>`
   });
   console.log('html', html);
   $container.innerHTML = html;
@@ -13,16 +14,16 @@ function buildHTML(data){
 
 
 fetch(BASE_URL)
-.then(response => response.json())
-.then(data => {
-  // console.log(data);
-  buildHTML(data);
-})
-.catch(error => console.log(error));
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data);
+    buildHTML(data);
+  })
+  .catch(error => console.log(error));
 // console.log('data', data);
 let data = {
   name: 'Brian',
-  message: 'another mountain to climb',
+  message: 'test',
 }
 
 fetch(BASE_URL, {
@@ -32,3 +33,15 @@ fetch(BASE_URL, {
   },
   body: JSON.stringify(data),
 });
+
+function deleteBtn(id){
+  fetch(`${BASE_URL}/${item._id}`, {
+  method: 'DELETE',
+  headers: {
+    'Content-type': 'application/json'
+  }
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(err => console.log(err))
+}
