c0ebb5013276b0dba1b9d672c526663d18aa9554
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-23 16:14:53 -0400

    have delete button working and the post method is dynamic

--- main.js
+++ main.js
@@ -6,42 +6,78 @@ let html = '';
 function buildHTML(data) {
   data.forEach(function(item) {
     console.log('item', item);
-    html += `<li><span>${item.name}</span>-${item.message} <button onclick="deleteBtn(${item._id})">Delete</button></li>`
+    const id = item._id;
+    // console.log('id', id);
+    html += `<li><span>${item.username}</span>:${item.message} <button type='button' data-id=${id} onclick="deleteBtn('${id}')">Delete</button><button type='button' onclick="editBtn('${id}')">Edit</button></li>`
+
   });
-  console.log('html', html);
+  // console.log('html', html);
   $container.innerHTML = html;
+
+  // document.querySelectorAll('button[type="button"]').forEach(button => button.addEventListener('click', deleteBtn));
+
 }
 
-
+//// Get
 fetch(BASE_URL)
   .then(response => response.json())
   .then(data => {
-    // console.log(data);
     buildHTML(data);
   })
   .catch(error => console.log(error));
-// console.log('data', data);
-let data = {
-  name: 'Brian',
-  message: 'test',
+// let data = {
+//   username: 'Brian',
+//   message: 'Test',
+// }
+
+function newData(event) {
+  event.preventDefault();
+  // console.log(event.target[0].value);
+  const data = {};
+  data.username = event.target[0].value;
+  data.message = event.target[1].value;
+  // POST
+  fetch(BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
 }
 
-fetch(BASE_URL, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-});
+const formEl = document.querySelector('form');
+formEl.addEventListener('submit', newData);
 
-function deleteBtn(id){
-  fetch(`${BASE_URL}/${item._id}`, {
-  method: 'DELETE',
-  headers: {
-    'Content-type': 'application/json'
-  }
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(err => console.log(err))
+/// PUT
+function editBtn(id) {
+
+  fetch(`${BASE_URL}/${id}`, {
+      method: 'Put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'Brian',
+        message: 'Static',
+      }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
 }
+/// DELETE
+function deleteBtn(id) {
+  // console.log('id', id);
+  // console.log(this);
+  // const id = event.target.dataset.id;
+  // console.log('id', id);
+  // console.log(data);
+  fetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+// fetch(BASE_URL).then((response) => response.json()).then((data)=> console.log(data));
